e4f63808d7263e24e0b416fd0817a1d01ea5c534
Julio Cesar G.E <juliogalvez90@gmail.com>
2022-09-21 17:48:29 -0500

    Code included in a Python Jupiter NoteBook

--- data_science.py
+++ data_science.py
@@ -1,10 +1,4 @@
 #-----------------------------------------------------
 # Julio Cesar Galvez Espinosa
 #-----------------------------------------------------
-import pandas as pd
-import os
 
-# 1) - Extracción del path de la data de pozos petroliferos.
-oil_wells = os.getcwd() + '/data_sets/Data15_9-19A-CORE_B.xlsx'
-
-core_data = pd.read_excel(oil_wells ,na_values=' ')
